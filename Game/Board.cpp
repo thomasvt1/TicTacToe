14f45d31f80e0bd37a53db00af08837bc7ee0342
thomasvt1 <thomasvt@me.com>
2018-01-16 14:57:49 +0100

    Board.cpp

--- Game/Board.cpp
+++ Game/Board.cpp
@@ -45,8 +45,8 @@ void Board::switchTurn()
 		turn = players[0];
 	else
 		turn = players[currentplayer + 1];
-	
-		
+
+
 }
 
 
@@ -90,22 +90,22 @@ string Board::winDetection()
 {
 	int k, l, p;
 	bool win = false;
-	for (int i = 0; i < 64; i += 4)
+	for (int l = 0; l < 64; l += 4)
 	{
-		//first time : k = 0 i = 0 l = 0		second time : k = 0 i = 4 l = 4
-		l = i;
-		if (board[l] == board[l + 1] && board[l] == board[l + 2] && board[l] == board[l + 3])
+		if (board[l] == board[l + 1] && board[l] == board[l + 2] && board[l] == board[l + 3])			//horizontal from front view
 		{
 			return board[l];
 		}
-		if (i>-1 && i<4 || i>15 && i<20 || i>31 && i<36 || i>47 && i<52)
+
+		if (l>-1 && l<4 || l>15 && l<20 || l>31 && l<36 || l>47 && l<52)												//vertical from front view
 		{
 			if (board[l] == board[l + 4] && board[l] == board[l + 8] && board[l] == board[l + 12])
 			{
 				return board[l];
 			}
 		}
-		if (i == 0 || i == 16 || i == 32 || i == 48)
+
+		if (l == 0 || l == 16 || l == 32 || l == 48)																	//crosses from front view
 		{
 			if ((board[l] == board[l + 5] && board[l] == board[l + 10] && board[l] == board[l + 15]) ||
 				(board[l + 3] == board[l + 6] && board[l] == board[l + 9] && board[l] == board[l + 12]))
@@ -114,32 +114,63 @@ string Board::winDetection()
 			}
 		}
 	}
-	return "";
-}
-
-/*
-string Board::winDetection()
-{
-	int k, l, p;
-	bool win = false;
-	for (int j = 0; j < 4; j++)
+	for (int j = 0; j < 16; j++)
 	{
-		k = j * 16;
-		for (int i = 0; i < 16; i += 4)
+		if (board[j] == board[j + 16] && board[j] == board[j + 32] && board[j] == board[j + 48])			//depth from front view
 		{
-			l = k + i;
-			if ((board[l] == board[l + 1] && board[l] == board[l + 2] && board[l] == board[l + 3]) ||
-				(board[l] == board[l + 4] && board[l] == board[l + 8] && board[l] == board[l + 12]) ||
-				(board[l] == board[l + 5] && board[l] == board[l + 10] && board[l] == board[l + 15]) ||
-				(board[l + 3] == board[l + 6] && board[l + 3] == board[l + 9] && board[l + 3] == board[l + 12]) ||
-				(board[l] == board[l + 16] && board[l] == board[l + 32] && board[l] == board[l + 48]) ||
-				(board[l] == board[l + 21] && board[l] == board[l + 42] && board[l] == board[l + 63]) ||
-				(board[12] == board[25] && board[12] == board[38] && board[12] == board[51]))
+			return board[j];
+		}
+
+		if (j == 0 || j == 4 || j == 8 || j == 12)
+		{
+			if (board[j] == board[j + 17] && board[j] == board[j + 34] && board[j] == board[j + 51])	//diagnal from above view
 			{
-				return board[l];
+				return board[j];
 			}
+		}
+
+		if (j == 3 || j == 7 || j == 11 || j == 16)
+		{
+			if (board[j] == board[j + 15] && board[j] == board[j + 30] && board[j] == board[j + 45])	//other diagnal from above view
+			{
+				return board[j];
+			}
+		}
+		if ((board[0] == board[21] && board[0] == board[42] && board[0] == board[63]) ||						//crosses through the cube
+			(board[3] == board[22] && board[3] == board[41] && board[3] == board[60]) ||
+			(board[12] == board[25] && board[12] == board[38] && board[12] == board[51]) ||
+			(board[15] == board[26] && board[15] == board[37] && board[15] == board[48])
+			)
+		{
+			return board[j];
 		}
 	}
 	return "";
 }
-*/
+
+	/*
+	string Board::winDetection()
+	{
+		int k, l, p;
+		bool win = false;
+		for (int j = 0; j < 4; j++)
+		{
+			k = j * 16;
+			for (int i = 0; i < 16; i += 4)
+			{
+				l = k + i;
+				if ((board[l] == board[l + 1] && board[l] == board[l + 2] && board[l] == board[l + 3]) ||
+					(board[l] == board[l + 4] && board[l] == board[l + 8] && board[l] == board[l + 12]) ||
+					(board[l] == board[l + 5] && board[l] == board[l + 10] && board[l] == board[l + 15]) ||
+					(board[l + 3] == board[l + 6] && board[l + 3] == board[l + 9] && board[l + 3] == board[l + 12]) ||
+					(board[l] == board[l + 16] && board[l] == board[l + 32] && board[l] == board[l + 48]) ||
+					(board[l] == board[l + 21] && board[l] == board[l + 42] && board[l] == board[l + 63]) ||
+					(board[12] == board[25] && board[12] == board[38] && board[12] == board[51]))
+				{
+					return board[l];
+				}
+			}
+		}
+		return "";
+	}
+	*/
